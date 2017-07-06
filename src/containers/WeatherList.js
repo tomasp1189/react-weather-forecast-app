eb8759c2f4077d7843d5b36f11559e33ce54eb1f
Tomas <tomasp1189@gmail.com>
2017-07-06 14:58:09 -0300

    Adding map and styles

--- src/containers/WeatherList.js
+++ src/containers/WeatherList.js
@@ -1,26 +1,23 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import Chart from '../components/Chart';
+import GoogleMap from '../components/GoogleMap';
 
 class WeatherList extends Component {
     renderWeather(cityData) {
         const name = cityData.city.name
-        const temps = cityData.list.map(weather => {
-            return weather.main.temp;
-        })
-        const pressures = cityData.list.map(weather => {
-            return weather.main.pressure;
-        })
-        const humidities = cityData.list.map(weather => {
-            return weather.main.humidity;
-        })
+        const temps = _.map(cityData.list.map(weather => { return weather.main.temp; }), (temp) => temp - 273);
+        const pressures = cityData.list.map(weather => { return weather.main.pressure; })
+        const humidities = cityData.list.map(weather => { return weather.main.humidity; })
+        const { lon, lat } = cityData.city.coord;
+
         return (
             <tr key={name}>
-                <td>{name}</td>
-                <td>{cityData.city.country}</td>
-                <td><Chart data={temps} color="blue" units="K"/></td>
-                <td><Chart data={pressures} color="red" units="hPa"/></td>
-                <td><Chart data={humidities} color="green" units="%"/></td>
+                <td className="col-md-2 align-middle"><GoogleMap lon={lon} lat={lat} /></td>
+                <td className="col-md-1 align-middle">{cityData.city.country}</td>
+                <td className="col-md-3 align-middle"><Chart data={temps} color="blue" units="C" /></td>
+                <td className="col-md-3 align-middle"><Chart data={pressures} color="red" units="hPa" /></td>
+                <td className="col-md-3 align-middle"><Chart data={humidities} color="green" units="%" /></td>
             </tr>
         );
     }
@@ -30,11 +27,11 @@ class WeatherList extends Component {
             <table className="table table-hover">
                 <thead>
                     <tr>
-                        <th>City</th>
-                        <th>Country</th>
-                        <th>Temperature (K)</th>
-                        <th>Pressure (hPa)</th>
-                        <th>Humidity (%)</th>
+                        <th className="col-md-2 align-middle">City</th>
+                        <th className="col-md-1 align-middle"> Country</th>
+                        <th className="col-md-3 align-middle">Temperature (C)</th>
+                        <th className="col-md-3 align-middle">Pressure (hPa)</th>
+                        <th className="col-md-3 align-middle">Humidity (%)</th>
                     </tr>
                 </thead>
                 <tbody>
